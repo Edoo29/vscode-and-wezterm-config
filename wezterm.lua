edbd1f7c3cd3d2cd8b0d7654d793669da165f084
Edo <155387587+Edoo29@users.noreply.github.com>
2025-08-25 14:59:55 +0200

    Update wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -1,46 +1,54 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-local grey = "#CCCCCC"
-local black = "#000000"
+local black = "#121212"
+local black_darker = "#090909"
 local white = "#FFFFFF"
+local red = "#BF616A"
+local blue = "#81A1C1"
 local transparent = "rgba(0, 0, 0, 0)"
 
 config.default_prog = { "pwsh.exe", "-NoLogo" }
-config.color_scheme = "Tokyo Night"
+config.color_scheme = "Ashes (dark) (terminal.sexy)"
 config.hide_tab_bar_if_only_one_tab = false
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
 config.window_decorations = "RESIZE"
-config.font = wezterm.font("Comic Mono")
-config.font_size = 12
+config.font = wezterm.font("ComicShannsMono Nerd Font")
+config.font_size = 13
 config.enable_tab_bar = true
 config.default_cursor_style = "SteadyBlock"
 config.show_new_tab_button_in_tab_bar = false
-config.window_background_opacity = 0.75
+config.cursor_blink_rate = 0
+config.window_background_opacity = 0.85
 config.tab_max_width = 50
 config.win32_system_backdrop = "Acrylic"
+
+config.window_background_image_hsb = {
+  brightness = 0.05,
+}
 
 config.colors = {
 	tab_bar = {
 		background = transparent,
 	},
 	cursor_bg = "#e8e8e8",
-	background = black,
+
+	background = black
 }
 
 wezterm.on("format-tab-title", function(tab)
 	local idx = " " .. tab.tab_index + 1 .. " "
 	if tab.is_active then
 		return {
-			{ Background = { Color = transparent } },
-			{ Foreground = { Color = white } },
+			{ Background = { Color = red } },
+			{ Foreground = { Color = black_darker } },
 			{ Text = idx },
 		}
 	else
 		return {
 			{ Background = { Color = transparent } },
-			{ Foreground = { Color = grey } },
+			{ Foreground = { Color = blue } },
 			{ Text = idx },
 		}
 	end
@@ -48,17 +56,17 @@ end)
 
 wezterm.on("update-right-status", function(window)
 	window:set_right_status(wezterm.format({
-		{ Background = { Color = transparent } },
+		{ Background = { Color = black_darker } },
 		{ Foreground = { Color = white } },
-		{ Text = "Hello, World! " },
+		{ Text = "" },
 	}))
 end)
 
 config.window_padding = {
-	left = 10,
-	right = 10,
-	top = 10,
-	bottom = 10,
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
 
 return config
